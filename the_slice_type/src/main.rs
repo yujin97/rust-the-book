f0333f0ad5928737c256857ff1272ddb6a694c5e
Eugene <eugene97@connect.hku.hk>
2023-04-17 22:37:03 +0900

    feat: use string slice

--- the_slice_type/src/main.rs
+++ the_slice_type/src/main.rs
@@ -1,9 +1,25 @@
-// main function
 fn main() {
-    // create a slice of i32
-    let slice: &[i32] = &[1, 2, 3];
-    // print the slice
-    println!("slice: {:?}", slice);
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    s.clear();
+
+    println!("size of {}'s first word: {}", s, word);
+
+    // string slices
+    let s1 = String::from("hello world");
+
+    let hello = &s1[0..5];
+    let world = &s1[6..11];
+
+    println!("{} {}", hello, world);
+
+    let s2 = String::from("immutable string");
+
+    let immutable_word = enhanced_first_word(&s2);
+
+    println!("the {}", immutable_word);
 }
 
 fn first_word(s: &String) -> usize {
@@ -16,4 +32,16 @@ fn first_word(s: &String) -> usize {
     }
 
     s.len()
+}
+
+fn enhanced_first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
