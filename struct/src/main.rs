8058d07c6e9a968c11fb538760613673d6f669d9
Eugene <eugene97@connect.hku.hk>
2023-04-18 23:27:36 +0900

    feat: try struct methods and associated functions

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,3 +1,5 @@
+use std::mem::size_of;
+
 fn main() {
     // the whole struct must be mutable
     let mut user1 = User {
@@ -19,6 +21,29 @@ fn main() {
     let origin = Point(0, 0, 0);
 
     let subject = AlwaysEqual;
+
+    let rec1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels", rec1.area());
+    println!("rect1 is {:?}", rec1);
+
+    let rec2 = Rectangle {
+        width: dbg!(30 * 3),
+        height: 70,
+    };
+
+    dbg!(&rec2);
+
+    let rec3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rec2? {}", rec1.can_hold(&rec2));
+    println!("Can rect1 hold rec3? {}", rec1.can_hold(&rec3));
 }
 
 struct User {
@@ -43,3 +68,26 @@ struct Point(i32, i32, i32);
 
 // unit-like struct
 struct AlwaysEqual;
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
