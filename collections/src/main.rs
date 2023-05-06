5ef2988e55586bde7ac9c822638ae1c6ef3d070d
Eugene <eugene97@connect.hku.hk>
2023-05-07 02:46:43 +0900

    fix: handle float number and multiple modes

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,14 +1,18 @@
 use std::collections::HashMap;
 
 fn main() {
-    let numbers = vec![7, 8, 9, 1, 5, 5, 5, 7, 8, 9, 9];
+    let numbers = vec![1, 8, 9, 8, 1, 4, 6, 5, 5, 6, 8, 9, 9, 4];
 
     let median_and_mode = find_median_and_mode(&numbers);
 
     println!("{:?}", median_and_mode);
 }
 
-fn find_median_and_mode(numbers: &Vec<i32>) -> Vec<i32> {
+fn find_median_and_mode(numbers: &Vec<i32>) -> Vec<f32> {
+    let mut ans = Vec::new();
+    if numbers.len() == 0 {
+        return ans;
+    }
     let mut owned_numbers = numbers.clone();
     owned_numbers.sort();
     let mut counts = HashMap::new();
@@ -16,25 +20,32 @@ fn find_median_and_mode(numbers: &Vec<i32>) -> Vec<i32> {
         0 => {
             let larger = numbers.len() / 2 - 1;
             let smaller = larger - 1;
-            owned_numbers[(smaller + larger) / 2]
+            let median = (owned_numbers[larger] + owned_numbers[smaller]) as f32 / 2.0;
+            median
         }
-        _ => owned_numbers[numbers.len() / 2],
+        _ => owned_numbers[numbers.len() / 2] as f32,
     };
+
+    ans.push(median);
 
     for number in &owned_numbers {
         let count = counts.entry(number.to_owned()).or_insert(0);
         *count += 1;
     }
 
-    let mut mode = owned_numbers.get(0).unwrap_or(&0).to_owned();
     let mut mode_count = 0;
 
-    for (key, value) in counts {
-        if value > mode_count {
-            mode_count = value;
-            mode = key;
+    for (_, value) in &counts {
+        if value > &mode_count {
+            mode_count = value.to_owned();
         }
     }
 
-    return vec![median, mode];
+    for (key, value) in counts {
+        if value == mode_count {
+            ans.push(key.clone() as f32);
+        }
+    }
+
+    return ans;
 }
