6169bf97e189c32fb4dfd944b1cba91634dad6fd
Eugene <eugene97@connect.hku.hk>
2023-06-06 21:41:00 +0900

    feat: add trait object example

---
+++ trait_object/src/lib.rs
@@ -0,0 +1,27 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: i32,
+    pub height: i32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Draw a Button!");
+    }
+}
