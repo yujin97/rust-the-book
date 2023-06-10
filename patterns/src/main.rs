bab1aa5f8b27ba281d70ceca87096f0e5988a854
Eugene <eugene97@connect.hku.hk>
2023-06-11 00:04:18 +0900

    feat: try pattern syntax

---
+++ patterns/src/main.rs
@@ -0,0 +1,61 @@
+fn main() {
+    // matching literals
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        _ => println!("anything"),
+    }
+
+    // matching named variables
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    // multiple patterns
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // matching ranges of values with ..=
+    let x = 5;
+
+    match x {
+        1..=5 => println!("oen through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    // destructuring
+
+    // struct
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+}
