7a067ef416c70719e62e86ad51bd67fff00ac645
Eugene <eugene97@connect.hku.hk>
2023-06-12 09:19:34 +0900

    feat: implement unsafe examples

---
+++ unsafe_rust/src/main.rs
@@ -0,0 +1,75 @@
+use std::slice;
+
+static HELLO_WORLD: &str = "Hello, world!";
+static mut COUNTER: u32 = 0;
+
+fn main() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is {}", *r1);
+        println!("r2 is {}", *r2);
+    }
+
+    let address = 0x012345usize;
+    let r = address as *const i32;
+
+    unsafe {
+        dangerous();
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let s = &mut v[..];
+
+    let (a, b) = s.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+
+    println!("name is {}", HELLO_WORLD);
+
+    add_to_count(3);
+
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
+}
+
+unsafe fn dangerous() {}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}
