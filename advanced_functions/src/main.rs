ec825c286c11f2acd6ae0b53527edc8a41d2aca6
Eugene <eugene97@connect.hku.hk>
2023-06-15 08:26:21 +0900

    feat: add advanced functions and closures examples

---
+++ advanced_functions/src/main.rs
@@ -0,0 +1,24 @@
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+enum Status {
+    Value(u32),
+    Stop,
+}
+
+fn _return_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn main() {
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is {}", answer);
+
+    let _list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+}
